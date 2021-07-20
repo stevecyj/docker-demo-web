bae45fdf3931af8ecbc8de874b9efd7a0d815639
steve_tsao <steve_tsao@neithnet.com>
2021-07-20 16:38:16 +0800

    remove mysql-db volume

--- db/sql-scripts/InsertData.sql
+++ db/sql-scripts/InsertData.sql
@@ -1,2 +1,9 @@
-INSERT INTO user (id, name, email) VALUES (0,'admin', 'admin@mail.com');
+INSERT INTO user (id, name, email) VALUES (100,'admin', 'admin@mail.com');
+INSERT INTO user (id, name, email) VALUES (101,'Oliver', 'Oliver@mail.com');
+INSERT INTO user (id, name, email) VALUES (102,'Harry', 'Harry@mail.com');
+INSERT INTO user (id, name, email) VALUES (103,'Charlie', 'Charlie@mail.com');
+INSERT INTO user (id, name, email) VALUES (104,'Thomas', 'Thomas@mail.com');
+INSERT INTO user (id, name, email) VALUES (105,'George', 'George@mail.com');
+INSERT INTO user (id, name, email) VALUES (106,'Isla', 'Isla@mail.com');
+INSERT INTO user (id, name, email) VALUES (107,'James', 'James@mail.com');
 
